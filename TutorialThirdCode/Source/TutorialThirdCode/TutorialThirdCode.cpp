52c58795585020b626c3f717c3318cbac062d374
y2jinc <y2jinc@gmail.com>
2014-07-21 16:39:28 +0900

    first turorial thirdcode for unrealengine

---
+++ TutorialThirdCode/Source/TutorialThirdCode/TutorialThirdCode.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#include "TutorialThirdCode.h"
+#include "TutorialThirdCode.generated.inl"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TutorialThirdCode, "TutorialThirdCode" );
+ 
